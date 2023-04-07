4826675f29aec6cf44b7c90eba0c4136a8134630
NastyaKuz <anastasiya3513@gmail.com>
2023-04-07 23:30:07 +0500

    Commit 1

---
+++ Lab5DBVersion3/Model/PatternObjectDB.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5DBVersion3.Model
+{
+    public class PatternObjectDB
+    {
+        public string Name { get; private set; }
+        public Dictionary<string, PatternPropertyDB> Properties { get; private set; }
+
+        public PatternObjectDB(string nameFile)
+        {
+            ReadFile(nameFile);
+        }
+
+        private void ReadFile(string nameFile)
+        {
+            string[] data = File.ReadAllLines(nameFile);
+            for (int i = 0; i < data.Length; i++)
+            {
+                string line = data[i];
+                if (line.Contains("name") && Name == null)
+                {
+                    Name = ReadQuotes(line);
+                }
+                if (line.Contains("columns"))
+                {
+                    Properties = ReadProperties(data, i);
+                }
+            }
+        }
+        private Dictionary<string, PatternPropertyDB> ReadProperties(string[] data, int indStart)
+        {
+            Dictionary<string, PatternPropertyDB> result = new Dictionary<string, PatternPropertyDB>();
+            for (int i = indStart + 1; i < data.Length; i++)
+            {
+                string line = data[i];
+                if (line.Contains('{'))
+                {
+                    PatternPropertyDB property = new PatternPropertyDB(data, i);
+                    result[property.Name] = property;
+                }
+            }
+            return result;
+        }
+        public static string ReadQuotes(string line)
+        {
+            int startSearch = line.IndexOf(':');
+            int start = line.IndexOf('"', startSearch) + 1;
+            int end = line.IndexOf('"', start);
+            return line.Substring(start, end - start);
+        }
+    }
+}
---
+++ Lab5DBVersion3/Model/PatternPropertyDB.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5DBVersion3.Model
+{
+    public class PatternPropertyDB
+    {
+        public string Name { get; private set; }
+        public PropertyType Type { get; private set; }
+
+        public PatternPropertyDB(string[] data, int indStart)
+        {
+            ReadProperty(data, indStart);
+        }
+
+        private void ReadProperty(string[] data, int indStart)
+        {
+            for (int i = indStart; i < data.Length; i++)
+            {
+                string line = data[i];
+                if (line.Contains('}'))
+                    break;
+                if (line.Contains("name"))
+                    Name = PatternObjectDB.ReadQuotes(line);
+                if (line.Contains("type"))
+                    ChoiceType(PatternObjectDB.ReadQuotes(line));
+            }
+        }
+
+        private void ChoiceType(string type)
+        {
+            switch (type)
+            {
+                case "int":
+                    Type = PropertyType.Int;
+                    break;
+                case "string":
+                    Type = PropertyType.String;
+                    break;
+                case "datatime":
+                    Type = PropertyType.DataTime;
+                    break;
+            }
+        }
+
+        public enum PropertyType
+        {
+            Int, String, DataTime
+        }
+    }
+}
